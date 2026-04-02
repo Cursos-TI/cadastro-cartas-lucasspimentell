50d82d76f0d558e4ff10236f9f1f32d6e26c1da2
Lucas Pimentel <132183112+lucasspimentell@users.noreply.github.com>
2026-04-02 22:57:51 +0000

    Fiz o desafio mestre

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -17,6 +17,7 @@ float pib1;
 int pontosTuristicos1;
 double densidadePopulacional1;
 double pibPerCapita1;
+int superPoder1;
 
 int carta2;
 char estado2;
@@ -28,6 +29,9 @@ float pib2;
 int pontosTuristicos2;
 double densidadePopulacional2;
 double pibPerCapita2;
+int superPoder2;
+
+int resultadoPopulacao,resultadoArea,resultadoPib,resultadoPontosTuristicos,resultadoDensidadePopulacional ,resultadoPibPerCapita,resultadoSuperPoder;
 
   // Área para entrada de dados
 
@@ -83,7 +87,9 @@ double pibPerCapita2;
       scanf("%d",&pontosTuristicos2);
 
   // Área para exibição dos dados da cidade
-    printf("\n Carta:%d \n Estado:%c \n Código: %s \n Nome da cidade: 1%s \n População: %.2f \n Área: %.2f Km² \n PIB: %.2f Reais\n Número de pontos Turísticos: %d \n Densidade populacional: %.2f hab/km²\n PIB per Capita: %.2f reais\n",carta1,estado1,codigo1,nomeCidade1,populacao1,area1,pib1,pontosTuristicos1,densidadePopulacional1 = populacao1/area1,pibPerCapita1 = pib1/populacao1);
+    printf("\n Carta:%d \n Estado:%c \n Código: %s \n Nome da cidade: 1%s \n População: %.2f \n Área: %.2f Km² \n PIB: %.2f Reais\n Número de pontos Turísticos: %d \n Densidade populacional: %.2f hab/km²\n PIB per Capita: %.2f reais\n",carta1,estado1,codigo1,nomeCidade1,populacao1,area1,pib1,pontosTuristicos1,densidadePopulacional1 = populacao1/area1,pibPerCapita1 = pib1/populacao1, superPoder1 = pibPerCapita1 + area1 + pib1 + pontosTuristicos1 + (int)(populacao1/area1) + (int)pibPerCapita1);
 
-    printf("\n Carta:%d \n Estado:%c \n Código: %s \n Nome da cidade: 1%s \n População: %.2f \n Área: %.2f Km² \n PIB: %.2f Reais\n Número de pontos Turísticos: %d \n Densidade populacional: %.2f hab/km²\n PIB per Capita: %.2f reais\n",carta2,estado2,codigo2,nomeCidade2,populacao2,area2,pib2,pontosTuristicos2,densidadePopulacional2 = populacao2/area2,pibPerCapita2 = pib1/populacao1);
+    printf("\n Carta:%d \n Estado:%c \n Código: %s \n Nome da cidade: 1%s \n População: %.2f \n Área: %.2f Km² \n PIB: %.2f Reais\n Número de pontos Turísticos: %d \n Densidade populacional: %.2f hab/km²\n PIB per Capita: %.2f reais\n",carta2,estado2,codigo2,nomeCidade2,populacao2,area2,pib2,pontosTuristicos2,densidadePopulacional2 = populacao2/area2,pibPerCapita2 = pib2/populacao2,superPoder2 = pibPerCapita2 + area2 + pib2 + pontosTuristicos2 + (int)(populacao2/area2) + (int)pibPerCapita2);
+
+    printf("\nComparação de Cartas: \nPopulação: Carta 1 venceu (%d)\nÁrea: Carta 1 venceu (%d)\nPIB: Carta 1 venceu (%d)\nPontos Turísticos: Carta 1 venceu (%d)\nDensidade Populacional: Carta 2 venceu (%d)\nPIB per Capita: Carta 2 venceu (%d)\nSuper Poder: Carta 1 venceu (%d)\n",resultadoPopulacao = populacao1 > populacao2 , resultadoArea = area1>area2 , resultadoPib = pib1>pib2 , resultadoPontosTuristicos = pontosTuristicos1>pontosTuristicos2,resultadoDensidadePopulacional = densidadePopulacional1<densidadePopulacional2, resultadoPibPerCapita = (int)pibPerCapita1> (int)pibPerCapita2,resultadoSuperPoder = (int)superPoder1>(int)superPoder2);
 } 
